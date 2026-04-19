46d2c5c03d815b5a54b11e7b152ffacf28207a3a
Allison Valdez <allison.snipe@gmail.com>
2026-04-18 17:07:29 -0700

    done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,2 +1,18 @@
 // Import necessary components and NavLink
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import "./NavBar.css";
+
+// Declare a function for NavBar() make sure to provide accessible navigation 
+function NavBar() {
+    return (
+        <nav className="navbar">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/shop">Shop</NavLink>
+            <NavLink to="/admin">Admin Portal</NavLink>
+        </nav>
+
+    );
+}
+
+// Make it globally accessible
+export default NavBar;
